4e6d2a79aba090365b00df4367135b53545e5d99
barchinsky <barchinsky@gmail.com>
2013-05-28 14:55:03 +0300

    Added dynamic texture load(according to tank power index)

--- models/TankBase.py
+++ models/TankBase.py
@@ -6,23 +6,36 @@ import pyglet
 # TankBase - base class for all tansk using in the game
 
 class TankBase(cocos.sprite.Sprite):
-    def __init__(self,path = "media/test.png",position=(200,200)):
-        super(TankBase,self).__init__(path,position)
+    def __init__(self,power,position=(200,200)):
+        super(TankBase,self).__init__("resources/tanks/0.png",position)
         self.x,self.y = position #base object position
         self.speed_x,self.speed_y = 2, 2 #tank speed
         self.hp = 100 # tank's health
         self.isAnemy = True # is tank anemy
-        self.power = 0 # tanks power level
+        self.path = "" #path to sprite
+        self.power = power # tanks power level
+        #load texture in agreement with tank power
+        if self.power == 0:
+            self.path = "resources/tanks/0.png"
+        elif self.power == 1:
+            self.path = "resources/tanks/1.png"
+        elif self.power == 2:
+            self.path = "resources/tanks/2.png"
+        elif self.power == 3:
+            self.path = "resources/tanks/3.png"
+
+        super(TankBase,self).__init__(self.path,position=(self.x,self.y))
+
         self.schedule(self.update)
 
     def update(self,obj): #update object
         pass
 
-    def move(self):
+    def move(self): #move object
         pos = self.x+self.speed_x,self.y+self.speed_y
         self.position = pos
 
-    def shoot(self,obj): # tank shoot
+    def shoot(self,obj): #tank shoots
         pass
 
     def damage(self,damage_point): # set tank damage
