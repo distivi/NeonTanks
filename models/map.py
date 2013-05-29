35b371fe5aae20698e755b9731ec9dea36d80c48
Stas Dymedyuk on Mac (work) <dymedyuk@gmail.com>
2013-05-29 23:54:25 +0300

    Add method to delete call from map

--- models/map.py
+++ models/map.py
@@ -1,5 +1,7 @@
 import cocos
 
+
+
 class Map(cocos.layer.ScrollingManager):	
 	def __init__(self,map_path):
 		super(Map, self).__init__()
@@ -8,28 +10,56 @@ class Map(cocos.layer.ScrollingManager):
 		bg_layer = resources_from_tmx['background']
 		self.add(bg_layer)
 
-		block_layer = resources_from_tmx['blocks']
-		self.add(block_layer)
+		self.block_layer = resources_from_tmx['blocks']
+		self.add(self.block_layer)
+
+
+		print self.block_layer.properties
 
 		# testing code
-		print block_layer
-		for rows in block_layer.cells:
+		print self.block_layer
+		for rows in self.block_layer.cells:
 			for cell in rows:
 				tile = cell.tile
 				if tile:				 
 					print "-->",cell
 					print tile.properties
 
-				#test_property = 'xp' in cell == True
-				#print test_property
-
-		cell00 = block_layer.get_cell(0,0)
-		cell25 = block_layer.get_cell(25,25)
+		cell00 = self.block_layer.get_cell(0,0)
+		cell25 = self.block_layer.get_cell(25,25)
 		
 		print cell00
 		print cell25
-	
-		# end testing code
+
+		self.block_x = 0
+		self.block_y = 0
+
+		self.schedule_interval(self.update, 1)
+		
+		
+
+	def update(self, dt, *args, **kwargs):   
+		self.deleteCellAtPosition((self.block_x, self.block_y)) 
+
+		self.block_y += 1
+		if self.block_y == 25:
+			self.unschedule(self.update)
+			
+		
+
+	def deleteCellAtPosition(self,position):		
+		cell = self.block_layer.get_cell(position[0],position[1])				
+		for rows in self.block_layer.cells:
+			if cell in rows:
+				print "remove ",cell				
+				rows.remove(cell)				
+				self.update_blocks()
+				return
+
+	def update_blocks(self):
+		self.remove(self.block_layer)
+		self.add(self.block_layer)
+
 
 
 
