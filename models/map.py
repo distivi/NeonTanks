023fc1a9ad8f73ef5d26a4efa99b06396d0b980e
Dimediuk Stas <dymedyuk@gmail.com>
2013-05-30 14:51:00 +0300

    add test method to hide all blocks from map

--- models/map.py
+++ models/map.py
@@ -13,52 +13,42 @@ class Map(cocos.layer.ScrollingManager):
 		self.block_layer = resources_from_tmx['blocks']
 		self.add(self.block_layer)
 
+		self.blocks = []
 
-		print self.block_layer.properties
 
 		# testing code
 		print self.block_layer
 		for rows in self.block_layer.cells:
 			for cell in rows:
-				tile = cell.tile
-				if tile:				 
-					print "-->",cell
-					print tile.properties
+				tile = cell.tile				
+				if tile:		
+					self.blocks.append(cell)										
+					#print tile.properties
 
-		cell00 = self.block_layer.get_cell(0,0)
-		cell25 = self.block_layer.get_cell(25,25)
+		#cell00 = self.block_layer.get_cell(0,0)
 		
-		print cell00
-		print cell25
-
-		self.block_x = 0
-		self.block_y = 0
-
-		# self.schedule_interval(self.update, 1)
+		self.schedule_interval(self.testHidingAllBlocksFromMap, 0.01)
 		
 		
 
-	def update(self, dt, *args, **kwargs):   
-		self.deleteCellAtPosition((self.block_x, self.block_y)) 
+	def testHidingAllBlocksFromMap(self, dt):
+		# test method
+		if len(self.blocks) > 0:			
+			cell = self.blocks[0]
+			#print cell.position
+			#print cell.position[0]/20," === ",cell.position[1]/20
 
-		self.block_y += 1
-		if self.block_y == 25:
-			self.unschedule(self.update)
+			self.block_layer.set_cell_opacity(cell.position[0] / 20, cell.position[1] / 20, 0.5)
+		  	self.blocks.remove(cell)
 			
 		
 
 	def deleteCellAtPosition(self,position):		
-		cell = self.block_layer.get_cell(position[0],position[1])				
-		for rows in self.block_layer.cells:
-			if cell in rows:
-				print "remove ",cell				
-				rows.remove(cell)				
-				self.update_blocks()
-				return
+		# will be dinamic removing block at position
+		pass
 
-	def update_blocks(self):
-		self.remove(self.block_layer)
-		self.add(self.block_layer)
+
+
 
 
 
