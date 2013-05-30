92d36ea65f9ca2379a0c323aecc8bffcd794d7f3
Den <Heroes@email.ua>
2013-05-30 20:55:39 +0300

    changed class

--- ui/handler_key.py
+++ ui/handler_key.py
@@ -1,33 +1,27 @@
-#!/usr/bin/env python
-#-*- coding:utf8 -*-
+#-*- coding: utf8 -*-
 
 import sys
 import cocos
 import pyglet
 from base_screen import BaseScreen
-#from main_menu import MenuScreen
 
 ESC = 65307
 
 class HandlerKey(BaseScreen):
-    
     is_event_handler = True
-
     def __init__(self):
         super( HandlerKey, self).__init__()
-
+        self.chars_pressed = set()
         self.direction = 'none'
-        self.text = cocos.text.Label("Olo", x=100,y=100)
+        self.text = cocos.text.Label("Test class HandlerKey", x=100,y=100)
         self.add(self.text)
 
-    def on_key_press(self,key,modifiers):
-    	if key == ESC:
-           self.text.element.text = "ESC"
-           print "ESC"
-        else:
-            #mainMenu = cocos.scene.Scene(MenuScreen())
-            #cocos.director.director.push(mainMenu)
-            self.text.element.text = pyglet.window.key.symbol_string(key)
+    def on_key_press(self, key, modifiers):
+       self.chars_pressed.add(key)
+       self.text.element.text = pyglet.window.key.symbol_string(key)
+        
+    def on_key_release(self, key, modifiers):
+       self.chars_pressed.remove(key)
  
-cocos.director.director.init(resizable=True, caption="Test handler key")
+cocos.director.director.init(resizable=True, caption="Handler key")
 cocos.director.director.run(cocos.scene.Scene( HandlerKey() ) )
