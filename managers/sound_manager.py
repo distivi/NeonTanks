5c55f915430556d807ab09af7bb6841bbe5c1dfa
barchinsky <barchinsky@gmail.com>
2013-06-04 18:55:36 +0300

    Set volume method define. Constructor changed.

--- managers/sound_manager.py
+++ managers/sound_manager.py
@@ -5,13 +5,26 @@ import pyglet
 import pygame
 
 class SoundManager:
-    def __init__(self,soundType):
-        self.soudType = soundType
+    def __init__(self,soundType,volume=0.5): # volume should be 0.0 -> 1.0
+        #self.soudType = soundType
         pygame.mixer.init()
-        if soundType == 0:
-            pygame.mixer.music.load("resources/sounds/GUNSHOT.WAV")
-        else:
-            pass
+        self.setVolume(volume)
+        #if soundType == 0: #shoot
+        #    pygame.mixer.music.load("GUNSHOT.WAV")
+        #else:
+        #    pass
 
-    def play(self):
+    def playShoot(self):
+        pygame.mixer.music.load("resources/sounds/GUNSHOT.WAV")
         pygame.mixer.music.play()
+
+    def playMoving(self):
+        pygame.mixer.music.load("resources/sounds/TANK_MOVE.WAV")
+        pygame.mixer.music.play()
+
+    def playTankDestroy(self):
+        pygame.mixer.music.load("resources/sounds/TANK_DESTROYED.WAV")
+        pygame.mixer.music.play()
+
+    def setVolume(self,volume): # value should be between 0.0 and 1.0
+        pygame.mixer.music.set_volume(volume)
