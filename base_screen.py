c50cbdeed826ffe917ecfbd2dff179f4a2b3a55c
kliw4utsky <kliw4utsky@gmail.com>
2013-06-07 18:26:06 +0300

    Add resours(button,sound)

--- base_screen.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import cocos
-
-class BaseScreen(cocos.layer.ColorLayer):	
-	def __init__(self):
-		super(BaseScreen, self).__init__(200,200,200,200)
-		self.win_width, self.win_height = cocos.director.director.get_window_size()		
