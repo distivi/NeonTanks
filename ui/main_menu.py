167d6ca8eb48fd8dcd5da4213dc3c8101f7fb957
kliw4utsky <kliw4utsky@gmail.com>
2013-06-11 19:45:26 +0400

    Add fon and buttons

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -15,25 +15,21 @@ class MenuScreen(BaseScreen):
 		
 
 	def create_layer(self):		
-		label = cocos.text.Label("Main Menu",
-			font_size = 16,
-			anchor_x = 'center',
-			anchor_y = 'center')		
-		label.position = self.win_width / 2, self.win_height - 20
-		self.add(label)
+		label =	cocos.sprite.Sprite("resources/fon/12.png", position = (self.win_width / 2, self.win_height / 2))
+		self.add(label,z=0)
 
 	def create_menu(self):
 		menu_list = []
-		menu_list.append(cocos.menu.MenuItem('Continue',self.on_continue))
-		menu_list.append(cocos.menu.MenuItem('New Game',self.on_new_game))
+		menu_list.append(cocos.menu.ImageMenuItem("resources/buttons/btn_continue.png",self.on_continue))
+		menu_list.append(cocos.menu.ImageMenuItem("resources/buttons/btn_newgame.png",self.on_new_game))
 		#menu_list.append(cocos.menu.MenuItem('Tank crash test', self.on_crashTest ))
-		menu_list.append(cocos.menu.MenuItem('Load',self.on_load))
-		menu_list.append(cocos.menu.MenuItem('Save',self.on_save))
-		menu_list.append(cocos.menu.MenuItem('Quit', self.on_quit))
+		menu_list.append(cocos.menu.ImageMenuItem("resources/buttons/btn_load.png",self.on_load))
+		menu_list.append(cocos.menu.ImageMenuItem("resources/buttons/btn_save.png",self.on_save))
+		menu_list.append(cocos.menu.ImageMenuItem("resources/buttons/btn_exit.png", self.on_quit))
 		menu = cocos.menu.Menu()
 		menu.create_menu(menu_list);
-		menu.position = 0, 0
-		self.add(menu)
+		menu.position = 0,-91
+		self.add(menu,z=1)
 
 	def on_continue(self):
 		print "on_continue"
